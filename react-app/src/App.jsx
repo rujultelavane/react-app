6967d1e1ec1520bbd5dcaa52e63dabbe77b9acf9
Rujul Telavane <rstelavane@gmail.com>
2026-06-24 17:59:06 -0400

    useEffect Hook implementation

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,25 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 const Card = ({title}) => {
-    const [hasLiked, setHasLiked] = useState(false); //state variable hasLiked with default value false
+  const[count, setCount]=useState(0);
+  const [hasLiked, setHasLiked] = useState(false); //state variable hasLiked with default value false
     //states are not consistent over browser reloads/rerenders
     //but when states are changed, react automatically rerenders the component to reflect the change onscreen
 
     //hook - special functions that let you tap into react features
+    useEffect(() => {
+      console.log("${title} has been liked: ${hasLiked}");
+    }, [hasLiked]); //dependency array (deps) -- only if hasLiked changed, then this useEffect will be called
+
+    // useEffect(()=>{
+    //   console.log("CARD RENDERED")
+    // }, []); //runs only when this component appears
+
+    //conditional rednering -- show different ui depending on diff criteria
 
   return (
-    <div className="card" >
-      <h2>{title}</h2>
+    <div className="card" onClick={()=> setCount(count+1)}>
+      <h2>{title} <br/> {count || null}</h2>
 
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? '❤️':'🤍'}

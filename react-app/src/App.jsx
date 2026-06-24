dce678173e146bbcad3cab982678dcfd56d40159
Rujul Telavane <rstelavane@gmail.com>
2026-06-23 23:35:41 -0400

    created like button state w/ functionality

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,19 @@
+import {useState} from 'react'
+
 const Card = ({title}) => {
+    const [hasLiked, setHasLiked] = useState(false); //state variable hasLiked with default value false
+    //states are not consistent over browser reloads/rerenders
+    //but when states are changed, react automatically rerenders the component to reflect the change onscreen
+
+    //hook - special functions that let you tap into react features
+
   return (
     <div className="card" >
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️':'🤍'}
+      </button>
     </div>
     
   )

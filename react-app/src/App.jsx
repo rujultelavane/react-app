57ba78e69cfe9e90c6eee024215ba2f7cb85b78b
Rujul Telavane <rstelavane@gmail.com>
2026-06-23 23:18:35 -0400

    showing that inline styling takes preference over external styling

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,13 @@
 const Card = ({title}) => {
   return (
-    <div>
+    <div className="card" style={{
+      border: '1px solid #4b5362',
+      padding: '20px',
+      margin: '10px',
+      backgroundColor: '#31363f',
+      borderRadius: '10px',
+      minHeight: '100px'
+    }}>
       <h2>{title}</h2>
     </div>
     
@@ -10,10 +17,8 @@ const Card = ({title}) => {
 
 const App = () => {
   return (
-    <div>
-      <h2>Functional Arrow Component</h2>
-
-      <Card title="Star Wars"/>
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} isCool={true}/>
       <Card title="Avatar"/>
       <Card title="The Lion King"/>
     </div>
